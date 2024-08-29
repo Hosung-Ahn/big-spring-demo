ecc3a5ce6cf0a0eb094cbe4b1265d35052f42027
HOSUNG <an3735297@pusan.ac.kr>
2024-08-29 15:16:13 +0900

    spring security config

--- demo/src/main/java/com/example/demo/config/SpringSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -1,12 +1,55 @@
 package com.example.demo.config;
 
+import com.example.demo.domain.EAuthority;
+import com.example.demo.security.exception.JwtAccessDeniedHandler;
+import com.example.demo.security.exception.JwtAuthenticationEntryPoint;
+import com.example.demo.security.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
-    
+    private final JwtFilter jwtFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.cors((cors) -> {
+        });
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((e) -> e
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler));
+
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/api/admin").hasAnyAuthority(EAuthority.ROLE_ADMIN.name())
+                .requestMatchers("/api/user").hasAnyAuthority(EAuthority.ROLE_USER.name())
+                .anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
